dd9eaeb792247f2e24bf82b649f88ec633fd3c05
preilly17 <patch.reilly1@gmail.com>
2025-10-01 10:27:28 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -38,7 +38,6 @@ const allowedOrigins = envConfiguredOrigins.size
 const corsOptions: CorsOptions = {
   origin: allowedOrigins,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
