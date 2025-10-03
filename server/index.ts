755d6df302f1c49a7c4848e32cace147be849eb4
AJF98 <ajfbusinesses@gmail.com>
2025-10-03 16:58:34 -0400

    Expand default CORS origins for activity requests

--- server/index.ts
+++ server/index.ts
@@ -24,8 +24,13 @@ const envConfiguredOrigins = new Set([
 
 const defaultOrigins = new Set([
   "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:4173",
+  "http://127.0.0.1:4173",
   "https://www.tripsyncbeta.com",
-  "https://tripsyncbeta.com"
+  "https://tripsyncbeta.com",
 ]);
 
 if (defaultClientUrl) {
